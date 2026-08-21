28973c66826d5f8d296b9405a1461916c86a456e
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-21 10:58:09 +0700

    add get_data

--- app/schema/confession.py
+++ app/schema/confession.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class ConfessionSchema:
     confession: str
-    authors: list[str]
     active: bool
     confession_id: str
-    post_time: dict[str, int]
-    same_post_count: int = 0
+    post_time: int
--- app/services/save_data.py
+++ app/services/save_data.py
@@ -3,7 +3,13 @@ from app.schema.confession import ConfessionSchema
 
 class SaveData:
 
-    @staticmethod
-    def save_to_db(confession_data: ConfessionSchema):
+    def __init__(self):
+        self.db = db.connet()
 
-        _ = db.connet().confession_doc
+    def save_to_db(self, confession_data: ConfessionSchema):
+
+        _if_exits = self.db.docs.find_one_and_update(
+            {"confession_id": confession_data.confession_id},
+            {"$inc": {"same_post_count": 1}},
+        )
+
