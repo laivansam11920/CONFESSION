265357e85daf300c59035d07231fcb40e6f8c04a
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-22 07:55:50 +0700

    add limiter to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 def create_app():
 
     app = Flask(__name__)
 
+    limiter = Limiter(get_remote_address, default_limits=["200 per day", "50 per hour"], storage_uri="memory://",)
+    limiter.init_app(app)
+
+
     @app.route("/")
+    @limiter.limit("10/day")
     def index():
         return "hello"
 
