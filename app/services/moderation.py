0f7ba2a43a7bdef3ec7bc1d451fcaca7e0254116
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-23 20:24:24 +0700

    add logic get confession

--- app/services/moderation.py
+++ app/services/moderation.py
@@ -19,12 +19,17 @@ class GenAIModeration(AiServices, ConfessionManager):
         try:
             confession = self.db.docs.find(
                 {"status": "pending", "send": False},
-                {"_id": 0, "confession": 1},
+                {"_id": 0, "confession": 1, "confession_id": 1},
             )
             list_confession = {}
 
             for docs in confession:
-                list_confession["confession"] = docs.get("confession", "")
+
+                confession_id = docs.get("confession_id", None)
+                confession = docs.get("confession", None)
+
+                if confession_id and confession:
+                    list_confession[confession_id] = confession
 
             return list_confession
         except Exception as e:
