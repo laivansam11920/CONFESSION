bea9e7b64a1eabe73f569a12e19bc53671154c5d
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-23 18:24:16 +0700

    re frontend

--- configs/configs.py
+++ configs/configs.py
@@ -20,6 +20,7 @@ class Settings(BaseSettings):
 
     CHANGE_GET_DATA_BY_WEB: bool = Field(True, alias="CHANGE_GET_DATA_BY_WEB")
     GET_EMAIL: bool = Field(False, alias="GET_EMAIL")
+    NAME_GROUP_USE_PROJECT: str = Field(default="", alias="NAME_GROUP_USE_PROJECT")
 
     BABEL_DEFAULT_LOCALE: str = Field(default="en", alias="BABEL_DEFAULT_LOCALE")
     BABEL_TRANSLATION_DIRECTORIES: str = Field(default="translations", alias="BABEL_TRANSLATION_DIRECTORIES")
