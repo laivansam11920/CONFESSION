5137039fcdd3e9f5fc5258184d3fa46c949bb23d
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-24 11:42:25 +0700

    add setting main

--- tests/ai_test_Res.py
+++ tests/ai_test_Res.py
@@ -2,4 +2,8 @@ from app.services.moderation import moderation
 from app.services.get_confession import get_confession
 from dataclasses import asdict
 
-res = moderation.check_confession(get_confession.get())
+tes_cfs = {
+    "1": "tối học thêm về vào nhà nghỉ không? ngủ ở đó 1 hôm"
+}
+
+res = moderation.check_confession(tes_cfs)
