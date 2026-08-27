2dd39440b6b155353502e8ae59c1fa237146490b
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-28 00:25:11 +0700

    add flask-csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_babel import Babel
+from flask_wtf.csrf import CSRFProtect
 
 from app.extensions.limiter import limiter
 from app.routes import register_blueprints
@@ -12,11 +13,12 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 def create_app() -> Flask:
 
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
     babel = Babel()
     limiter.init_app(app)
     app.config.from_object(Config)
+    CSRFProtect(app)
     babel.init_app(app, locale_selector=get_locale)
     register_blueprints(app)
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 
     return app
