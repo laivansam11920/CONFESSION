eb737aaaed1f1779ad19ab648fd9b9e977c9e555
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-28 12:32:46 +0700

    del test bp

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -5,4 +5,3 @@ from .routes import *
 def register_blueprints(app: Flask):
     app.register_blueprint(main_route)
     app.register_blueprint(get_data)
-    app.register_blueprint(test)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 
 from app.extensions.limiter import limiter
 
 main_route: Blueprint = Blueprint("main_route", __name__)
 get_data: Blueprint = Blueprint("get_data", __name__)
-
-test: Blueprint = Blueprint("test", __name__)
 
 @main_route.route("/")
 @limiter.limit("10/day")
@@ -20,9 +18,3 @@ def get_confession():
 
     return get_data_web.get_data()  # type: ignore
 
-@test.route('/debug-ip')
-def debug_ip():
-    return {
-        "remote_addr": request.remote_addr,
-        "x_forwarded_for": request.headers.get('X-Forwarded-For')
-    }
