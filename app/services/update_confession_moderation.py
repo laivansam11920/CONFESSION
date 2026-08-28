9cdea2c882e69233ff2ba62b4528182fe7031e4c
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-28 10:20:20 +0700

    fix path

--- app/services/update_confession_moderation.py
+++ app/services/update_confession_moderation.py
@@ -1,4 +1,4 @@
-from utils.logger import console
+from app.utils.logger import console
 from .moderation import moderation
 from .get_confession import GetConfession as Confession
 from app.extensions.threads import executor
