ad627fe53e33282bf4b3d5fd39b2e71d8a15540c
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-28 12:11:05 +0700

    add test routes

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 def create_app() -> Flask:
 
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_proto=1, x_host=1)
     babel = Babel()
     limiter.init_app(app)
     app.config.from_object(Config)
