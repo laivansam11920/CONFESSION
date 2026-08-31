50aafe28e56b15214ef8bcfd39ffc7106d35c728
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-01 00:02:18 +0700

    re schema

--- app/schema/confession.py
+++ app/schema/confession.py
@@ -8,12 +8,16 @@ __all__ = ["ConfessionSchema"]
 class ConfessionSchema:
     confession: str
     confession_id: str
+
     post_time: int
-    email: list
     admin_comment: str = ""
-    safe_to_post: bool = False
+
+    email: list = field(default_factory=list)
     same_post_count: int = 0
+
+    safe_to_post: bool = False
     status: str = "pending"
     send: bool = False
+
     ai_data: dict = field(default_factory=dict)
     user_tracking_data: list[str] = field(default_factory=list)
