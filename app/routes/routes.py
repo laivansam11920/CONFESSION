4727f7f8e88d3f7dcbe86ce26fc4c4db181cc002
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-01 03:05:53 +0700

    add try-catch

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 from app.extensions.limiter import limiter
 from app.extensions.crfs import crfs
+from app.utils.logger import console
 
 main_route: Blueprint = Blueprint("main_route", __name__)
 get_data: Blueprint = Blueprint("get_data", __name__)
@@ -19,17 +20,24 @@ def index():
 
 @get_data.post("/submit-confession")
 def get_confession():
-    from app.controller.get_data import get_data_web
+    try:
+        from app.controller.get_data import get_data_web
 
-    return get_data_web.get_data()  # type: ignore
-
+        return get_data_web.get_data()  # type: ignore
+    except Exception as e:
+        console.error(e)
+        return {"msg": "this feature is not available"}
 
 @get_data.post("/submit-confession-form")
 @crfs.exempt
 def get_confession_form():
-    from app.controller.get_data import get_data_google
+    try:
+        from app.controller.get_data import get_data_google
 
-    return get_data_google.get_data()  # type: ignore
+        return get_data_google.get_data()  # type: ignore
+    except Exception as e:
+        console.error(e)
+        return {"msg": "this feature is not available"}
 
 
 @ping.route("/ping")
