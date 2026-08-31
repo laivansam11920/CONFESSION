dfb71f6a14e296645f6bc14dfc1bc2b61a9d8fc5
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-31 18:27:28 +0700

    del docs

--- app/utils/del_docs.py
+++ app/utils/del_docs.py
@@ -1,14 +1,21 @@
 from app.database import db
 
 import functools
+import time
 
 def del_docs(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
 
+        one_week_ago = time.time() - (7 * 24 * 60 * 60)
+
         db.docs.delete_many({
-            "send": True,
             "ai_data.uncertain": False,
+            "post_time": {"$lt": one_week_ago},
+            "$or": [
+                {"send": True},
+                {"send": False, "safe_to_post": False}
+            ]
         })
 
         return func(*args, **kwargs)
