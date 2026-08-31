bbcc2bea9eb46dc61f02d2af73dc6ecdb3aab8e6
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-01 02:53:58 +0700

    add extension file

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
-from flask_babel import Babel
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.extensions.limiter import limiter
 from app.extensions.crfs import crfs
+from app.extensions.babel import babel
 from app.routes import register_blueprints
 from .utils.change_lang import get_locale
 from configs import Config
-
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app() -> Flask:
 
     app = Flask(__name__)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_proto=1, x_host=1)
-    babel = Babel()
+    app.config.from_object(Config)
     limiter.init_app(app)
     crfs.init_app(app)
-    app.config.from_object(Config)
     babel.init_app(app, locale_selector=get_locale)
     register_blueprints(app)
 
