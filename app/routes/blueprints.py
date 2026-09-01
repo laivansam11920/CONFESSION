6ca6a6266acbfa57cb8bc59b8a779f49436b2980
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-02 03:00:06 +0700

    add range register route

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -4,7 +4,12 @@ from .routes import *
 
 def register_blueprints(app: Flask):
 
-    all_routes: list[Blueprint] = [main_route, get_data, ping, testing_route]
+    all_routes: list[Blueprint] = [
+        main_route,
+        get_data,
+        ping,
+        testing_route,
+    ]
 
     for route in all_routes:
         app.register_blueprint(route)
