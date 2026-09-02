b832ecb28da6b5373e5aa6663213193bc562c378
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-02 19:56:06 +0700

    add test route

--- app/services/del_post/del_post_facebook.py
+++ app/services/del_post/del_post_facebook.py
@@ -3,3 +3,12 @@ mô tả tính năng: xóa bài fb khi admin nhập key vào comment fb
 khi admin nhập key thì key đó sẽ hủy ngay lập tức
 khi truy vấn db bắt buộc phải dùng findOneAndUpdate
 """
+
+from .create_key_del import CreateKeyDelPost as KeyDelete
+from app.database import db
+from app.utils.check_similar import is_similar
+
+class DelPostFacebook:
+
+    def del_post_facebook(self):
+        ...
