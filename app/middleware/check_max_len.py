1716d19342fb737bb15831fa6c6d6fb87e3ac91a
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-04 13:44:41 +0700

    re mess

--- app/middleware/check_max_len.py
+++ app/middleware/check_max_len.py
@@ -1,21 +1,24 @@
 from flask_babel import Babel, gettext as _
 from app.schema.ReturnSchema import ReturnSchema
-
+from app.utils.logger import console
 
 def check_max_len(text: str, max_len: int, min_len: int) -> ReturnSchema:
-    l = len(text)
+    try:
+        l = len(text)
 
-    if l > max_len:
+        if l > max_len:
+            return ReturnSchema(
+                msg=_("Confession của bạn quá dài so với yêu cầu hệ thống!"),
+            )
+
+        if l <= min_len:
+            return ReturnSchema(
+                msg=_("Confession của bạn quá ngắn so với yêu cầu hệ thống!")
+            )
+
         return ReturnSchema(
-            success=False,
-            msg=_("Confession của bạn quá dài so với yêu cầu hệ thống!"),
+            success=True,
         )
-
-    if l <= min_len:
-        return ReturnSchema(
-            success=False, msg=_("Confession của bạn quá ngắn so với yêu cầu hệ thống!")
-        )
-
-    return ReturnSchema(
-        success=True,
-    )
+    except Exception as e:
+        console.error(e)
+        return ReturnSchema()
