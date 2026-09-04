894c86a09d57fe803718c9996e7acdac0a84266b
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-04 22:53:15 +0700

    add custom rate limit

--- app/extensions/limiter.py
+++ app/extensions/limiter.py
@@ -1,9 +1,11 @@
 from flask_limiter import Limiter
+
 from app.utils.get_client_tracking import get_client_tracking
+from configs import Config
 
 limiter = Limiter(
     key_func=get_client_tracking,
     default_limits=["10 per 10 minutes"],
-    storage_uri="memory://",
+    storage_uri=Config.REDIS_URL or "memory://",
 )
 __all__ = ["limiter"]
