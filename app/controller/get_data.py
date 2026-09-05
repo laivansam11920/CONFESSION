e726ace4c48fcf66737142a4711823d0d7321a91
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-05 18:38:32 +0700

    add 200

--- app/controller/get_data.py
+++ app/controller/get_data.py
@@ -49,7 +49,7 @@ class GetDataGoogleForm(GetData):
     @check_input_data
     def get_data(
         self, email: str = "", confession: str = "", is_sponsor: bool = False
-    ) -> dict:
+    ) -> tuple[dict, int]:
 
         post_time_reqs = request.form.get("post_time_reqs") or ""
         use_tag_cfs_reqs = request.form.get("use_tag_cfs_reqs") or ""
@@ -70,7 +70,7 @@ class GetDataGoogleForm(GetData):
 
         res: ReturnSchema = Confession.save_cfs(data)
 
-        return {"success": res.success, "msg": res.msg}
+        return {"success": res.success, "msg": res.msg}, 200
 
 
 if Config.CHANGE_GET_DATA_BY_WEB:
