bab04015cbe1b4de87c52429024745e237eb1a52
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-19 12:19:30 +0700

    add try-catch

--- app/controller/get_data.py
+++ app/controller/get_data.py
@@ -1,12 +1,13 @@
 from app.base import GetData
 from app.schema.confession import ConfessionSchema
+from app.schema.ReturnSchema import ReturnSchema
 from app.services.save_cfs.save_cfs import SaveConfession as Confession
 from app.utils.return_home import home
+from app.utils.logger import console
 from app.validation.check_input_data import check_input_data
-from app.schema.ReturnSchema import ReturnSchema
 from configs import Config
 
-from flask import flash, Response, request
+from flask import flash, Response
 
 import uuid
 import time
@@ -25,25 +26,30 @@ class GetDataWeb(GetData):
         post_time_reqs: str = "",
         use_tag_cfs_reqs: str = "",
     ) -> Response:
+        try:
 
-        data = ConfessionSchema(
-            confession=confession,
-            email=[
-                email,
-            ],
-            confession_id=str(uuid.uuid4()),
-            post_time=int(time.time()),
-            is_sponsor=is_sponsor,
-            sponsor_requirements={
-                "post_time_reqs": post_time_reqs,
-                "use_tag_cfs_reqs": use_tag_cfs_reqs,
-            },
-        )
+            data = ConfessionSchema(
+                confession=confession,
+                email=[
+                    email,
+                ],
+                confession_id=str(uuid.uuid4()),
+                post_time=int(time.time()),
+                is_sponsor=is_sponsor,
+                sponsor_requirements={
+                    "post_time_reqs": post_time_reqs,
+                    "use_tag_cfs_reqs": use_tag_cfs_reqs,
+                },
+            )
 
-        res: ReturnSchema = Confession.save_cfs(data)
+            res: ReturnSchema = Confession.save_cfs(data)
 
-        flash(res.msg, res.status)
-        return home()
+            flash(res.msg, res.status)
+            return home()
+
+        except Exception as e:
+            console.error(e)
+            return home()
 
 
 class GetDataGoogleForm(GetData):
@@ -57,24 +63,29 @@ class GetDataGoogleForm(GetData):
         post_time_reqs: str = "",
         use_tag_cfs_reqs: str = "",
     ) -> tuple[dict, int]:
+        try:
 
-        data = ConfessionSchema(
-            confession=confession,
-            email=[
-                email,
-            ],
-            confession_id=str(uuid.uuid4()),
-            post_time=int(time.time()),
-            is_sponsor=is_sponsor,
-            sponsor_requirements={
-                "post_time_reqs": post_time_reqs,
-                "use_tag_cfs_reqs": use_tag_cfs_reqs,
-            },
-        )
+            data = ConfessionSchema(
+                confession=confession,
+                email=[
+                    email,
+                ],
+                confession_id=str(uuid.uuid4()),
+                post_time=int(time.time()),
+                is_sponsor=is_sponsor,
+                sponsor_requirements={
+                    "post_time_reqs": post_time_reqs,
+                    "use_tag_cfs_reqs": use_tag_cfs_reqs,
+                },
+            )
 
-        res: ReturnSchema = Confession.save_cfs(data)
+            res: ReturnSchema = Confession.save_cfs(data)
 
-        return {"success": res.success, "msg": res.msg}, 200
+            return {"success": res.success, "msg": res.msg}, 200
+
+        except Exception as e:
+            console.error(e)
+            return {"success": False, "msg": ""}, 500
 
 
 if Config.CHANGE_GET_DATA_BY_WEB:
