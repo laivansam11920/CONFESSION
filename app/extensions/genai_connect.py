63c0ea6ffc88a632f5e7ba2ac983a7c04434d735
Lai Van Sam <samvasang1192011@gmail.com>
2026-09-25 19:46:55 +0700

    add try-catch

--- app/extensions/genai_connect.py
+++ app/extensions/genai_connect.py
@@ -1,9 +1,13 @@
 from google.genai import types, Client
 
 from configs import Config
+from app.utils.logger import console
 
 __all__ = ["client"]
 
-client: Client = Client(
-    api_key=Config.GOOGLE_AI_API_KEY,
-)
+try:
+    client: Client = Client(
+        api_key=Config.GOOGLE_AI_API_KEY,
+    )
+except Exception as e:
+    console.error(e)
